6bdc9b71b67f7e990c8b02345a0941d1eec5466a
Trance-0 <60459821+Trance-0@users.noreply.github.com>
2020-11-12 20:40:11 +0800

    2020.11.12

---
+++ src/C6/H01152.java
@@ -0,0 +1,18 @@
+package C6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class H01152 {
+	private final boolean[]
+	public H01152() throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+	}
+
+	public static void main(String[] args) throws IOException {
+		H01152 a = new H01152();
+	}
+}
