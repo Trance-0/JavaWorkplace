9efca3f047d19e33321fdfca89c227ecf6754450
Trance-0 <60459821+Trance-0@users.noreply.github.com>
2020-11-19 21:46:00 +0800

    2020.11.19

--- src/C9/H01067.java
+++ src/C9/H01067.java
@@ -5,59 +5,78 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 class H01067 {
-	private String beads;
+	private String[] beads;
 	private int length;
-	private int currentbeads;
-	private int lastbeads;
 	private int finalans;
-	private String temp = "w";
-	private String c;
-	private int finalindex;
+	private String lbead;
+	private String cbead;
+	private int b;
+	private int f;
 
 	public H01067() throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		length = Integer.parseInt(br.readLine());
-		beads = br.readLine();
+		beads = new String[length];
+		String temp = br.readLine();
 		for (int i = 0; i < length; i++) {
-			c = beads.substring(i, i + 1);
-			if (temp.compareTo("w") == 0 && c.compareTo("w") != 0) {
-				temp = c;
-			}
-			if (c.compareTo(temp) == 0 || c.compareTo("w") == 0) {
-				currentbeads++;
-			} else {
-				finalans = Math.max(currentbeads + lastbeads, finalans);
-				System.out.println(currentbeads + " " + temp + " i= " + i);
-				lastbeads = currentbeads;
-				while (getlastbeads(i).compareTo("w") == 0) {
-					if (i == 0) {
-						i = beads.length();
-					}
-					i--;
-				}
-				currentbeads = 1;
-				temp = c;
-			}
-			if (finalindex > 2 * beads.length()) {
-				break;
-			}
-			if (i == length - 1) {
-				i = 0;
-			}
-			if (currentbeads == length) {
-				finalans = length;
-				break;
-			}
-			finalindex++;
+			beads[i] = temp.substring(i, i + 1);
 		}
-		System.out.println(finalans);
+		lbead = beads[length - 1];
+		for (int i = 0; i < length; i++) {
+			if (beads[i].compareTo("w") != 0) {
+				cbead = beads[i];
+				if (isdifferent(lbead, cbead)) {
+//					System.out.println(
+//							"i=" + i + " forward " + countforward(i, cbead) + " backward " + countbackward(i, lbead));
+					finalans = Math.max(countforward(i, cbead) + countbackward(i, lbead), finalans);
+				}
+				lbead = cbead;
+			}
+		}
+		if (finalans == 0 || finalans > length) {
+			System.out.println(length);
+		} else {
+			System.out.println(finalans);
+		}
 	}
 
-	private String getlastbeads(int index) {
-		if (index == 0) {
-			index = beads.length();
+	private int countforward(int i, String color) {
+		int count = 0;
+		while (beads[i].compareTo(color) == 0 || beads[i].compareTo("w") == 0) {
+			i++;
+			if (i == length) {
+				i = 0;
+			}
+			if (count > length) {
+				return -1;
+			}
+			count++;
 		}
-		return beads.substring(index - 1, index);
+		return count;
+	}
+
+	private int countbackward(int i, String color) {
+		int count = 0;
+		if (i == 0) {
+			i = length;
+		}
+		i--;
+		while (beads[i].compareTo(color) == 0 || beads[i].compareTo("w") == 0) {
+			if (i == 0) {
+				i = length;
+			}
+			if (count > length) {
+				return -1;
+			}
+			i--;
+			count++;
+		}
+		return count;
+	}
+
+	private boolean isdifferent(String lbead2, String cbead2) {
+		return (lbead.compareTo("r") == 0 && cbead.compareTo("b") == 0)
+				|| (lbead.compareTo("b") == 0 && cbead.compareTo("r") == 0);
 	}
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
--- src/C9/H258a.java
+++ src/C9/H258a.java
@@ -1,5 +1,59 @@
 package C9;
 
-public class H258a {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class H258a {
+	private String line;
+	private int ledge;
+	private int redge;
+	private int firdis;
+	private int secdis;
+	private int length;
+
+	public H258a() throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		length = Integer.parseInt(br.readLine());
+		line = br.readLine();
+		int templength = 0;
+		for (int i = 0; i < length; i++) {
+			if (line.substring(i, i + 1).compareTo("1") == 0) {
+				if (templength == i) {
+					ledge = templength;
+				}
+				if (firdis <= templength) {
+					secdis = firdis;
+					firdis = templength;
+				}
+				if (i == length - 1) {
+					redge = templength;
+				}
+			} else {
+				templength++;
+			}
+		}
+		// in edge
+		int opt1 = ledge - 1;
+		int opt2 = redge - 1;
+		// in first dis
+		int opt3 = firdis / 3;
+		// in second first dis
+		int opt4 = (secdis - 1) / 2;
+		int opt5 = (firdis - 1) / 2;
+		int result = 0;
+		if (ledge == length) {
+			result = length - 2;
+		} else if (ledge > 3 * firdis + 1) {
+			result = (ledge - 1) / 2;
+//		}else if() {
+
+		}
+
+		System.out.println(result);
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		H258a a = new H258a();
+	}
 }
