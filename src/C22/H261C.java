12273653ec13566dc3917cf3f77844137af6343e
Trance-0 <60459821+Trance-0@users.noreply.github.com>
2021-01-03 17:45:59 +0800

    2020.1.3

--- src/C22/H261C.java
+++ src/C22/H261C.java
@@ -3,15 +3,16 @@ package C22;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
+
 public class H261C {
-    private class node {
+    private class node implements Cloneable{
         private int value;
         private ArrayList<Integer> path;
-        private int parent;
+        private node parent;
+
         public node(int a) {
             value = a;
             path = new ArrayList<Integer>();
@@ -20,21 +21,26 @@ public class H261C {
         public void add(int i) {
             path.add(i);
         }
-        public void setparent(int p){
-parent=p;
+
+        public void setparent(node p) {
+            parent = p;
         }
     }
 
     private node[] nodes;
     private int length;
+    private int sum;
 
     public H261C() throws NumberFormatException, IOException {
+        sum=0;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         length = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
+        nodes=new node[length];
         for (int i = 0; i < length; i++) {
             nodes[i] = new node(Integer.parseInt(st.nextToken()));
+            System.out.println(nodes[i].value);
         }
         for (int i = 0; i < length - 1; i++) {
             st = new StringTokenizer(br.readLine());
@@ -43,28 +49,44 @@ parent=p;
             nodes[x].add(y);
             nodes[y].add(x);
         }
+        
+    
         for (int i = 0; i < length; i++) {
             node root = nodes[i];
+            root.setparent(null);
             boolean[] isinlist = new boolean[length];
             isinlist[i] = true;
-            ArrayList<node> memo = new ArrayList<node>();
-            memo.add(root);
+            int rootvalue=root.value;
+            ArrayList<Integer> memo = new ArrayList<Integer>();
+            memo.add(i);
             int pro = 0;
             int size = 1;
             while (pro < size) {
-                root = memo.get(pro);
+                root = nodes[memo.get(pro)];
                 ArrayList<Integer> child = root.path;
                 for (int k : child) {
                     if (!isinlist[k]) {
+                        nodes[k].setparent(root);
                         isinlist[k] = true;
-                        memo.add(nodes[k]);
+                        memo.add(k);
+                        size++;
                     }
                 }
+                pro++;
             }
-            for(int f=memo.size()-1;f<=0;f--){
-
+            for (int f = size- 1; f >0; f--) {
+                node temp=nodes[memo.get(f)];
+                temp.parent.value=(temp.parent.value+(12-temp.value))%12;
+            }
+            System.out.println(i+"is ok"+rootvalue+"rootvalue"+memo.get(0)+"equal");
+            if(nodes[memo.get(0)].value==0||nodes[memo.get(0)].value==1){
+                sum++;
+            }
+            for(node a:nodes){
+                System.out.println(a.value);
             }
         }
+        System.out.println(sum);
     }
 
     public static void main(String[] args) throws NumberFormatException, IOException {
