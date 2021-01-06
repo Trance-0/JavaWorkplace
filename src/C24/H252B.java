eb6d2f15421f429b02805be7c5c55c34b9ef3974
Trance <wuzheyuan.86@gmail.com>
2021-01-06 13:44:43 +0800

    2021.1.7

--- src/C24/H252B.java
+++ src/C24/H252B.java
@@ -6,15 +6,40 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class H252B {
-    private int N;
-    private int M;
-    private int G;
-    private int X;
+    private long N;
+    private long M;
+    private long K;
+    private long X;
 
     public H252B() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        long i = 1;
+        long j = 10000000;
+        long X = (i + j) / 2;
+        while (i != j) {
+            if (works(X)) {
+                i = X;
+            } else {
+                j = X - 1;
+            }
+            System.out.println("i: " + i + " j: " + j);
+            X = (i + j + 1) / 2;
+        }
+        System.out.println(X);
     }
+
+    private boolean works(long x2) {
+        System.out.println("time=" + (Math.log(x2) / Math.log(N - M * x2) + x2));
+        if (Math.log(x2) / Math.log(N - M * x2) + x2 <= K) {
+            return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) throws IOException {
         H252B a = new H252B();
     }
