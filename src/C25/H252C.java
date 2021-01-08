11e403908b941997c7d995e5e96bd7b706e3ab4c
Trance-0 <60459821+Trance-0@users.noreply.github.com>
2021-01-08 13:48:01 +0800

    2020.1.8

---
+++ src/C25/H252C.java
@@ -0,0 +1,18 @@
+package C25;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class H252C {
+    private int warmholewidth;
+    private int 
+    public H252C() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+    }
+    public static void main(String[] args) throws IOException {
+        H252C a = new H252C();
+    }
+}
