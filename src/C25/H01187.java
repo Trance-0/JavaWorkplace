c1117f4ce150239bdb2eadc1b9217b699c9da5ba
Trance <wuzheyuan.86@gmail.com>
2021-01-13 12:00:37 +0800

    2021.1.13

--- src/C25/H01187.java
+++ src/C25/H01187.java
@@ -3,14 +3,23 @@ package C25;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
 import java.util.StringTokenizer;
 
 public class H01187 {
+    private int[] root;
+    private int n;
+    private int m;
+
     public H01187() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        while (n != 0 && m != 0) {
+            st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+        }
     }
 
     public static void main(String[] args) throws IOException {
