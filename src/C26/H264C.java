19f1c7a2cc516f6276a2503f2209a001e41a6eff
Trance <wuzheyuan.86@gmail.com>
2021-01-22 07:40:44 +0800

    2021.1.22

--- src/C26/H264C.java
+++ src/C26/H264C.java
@@ -1,5 +1,44 @@
 package C26;
 
-public class H264C {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class H264C {
+    private long p;
+    private long q;
+    private int n;
+
+    public H264C() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            p = div(Long.parseLong(st.nextToken()));
+            q = div(Long.parseLong(st.nextToken()));
+            if (p % q == 0) {
+                System.out.println("Finite");
+            } else {
+                System.out.println("Infinite");
+            }
+        }
+    }
+
+    private long div(long l) {
+        while (l % 2 == 0) {
+            l /= 2;
+        }
+        while (l % 5 == 0) {
+            l /= 5;
+        }
+        return l;
+    }
+
+    public static void main(String[] args) throws IOException {
+        H264C a = new H264C();
+    }
 }
--- src/C26/H264D.java
+++ src/C26/H264D.java
@@ -1,5 +1,56 @@
 package C26;
 
-public class H264D {
+import java.io.BufferedReader;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class H264D {
+    private int n;
+    private int[] city;
+    private int[] tele;
+
+    public H264D() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        city = new int[n];
+        tele = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            city[i] = Integer.parseInt(st.nextToken()) - 1;
+        }
+        // printlist(city);
+        for (int i = 0; i < n; i++) {
+            int step = 0;
+            LinkedList<Integer> d = new LinkedList<Integer>();
+            d.add(0);
+            int layersize = 1;
+            boolean end = false;
+            while (true) {
+                step++;
+                for (int j = 0; j < layersize; j++) {
+
+                }
+                layersize = d.size();
+            }
+            tele[i] = step;
+        }
+        printlist(tele);
+    }
+
+    private void printlist(int[] t) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(t[0]);
+        for (int i = 1; i < t.length; i++) {
+            sb.append(" ");
+            sb.append(t[i]);
+        }
+        System.out.println(sb.toString());
+    }
+
+    public static void main(String[] args) throws IOException {
+        H264D a = new H264D();
+    }
 }
