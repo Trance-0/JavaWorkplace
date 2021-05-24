0530a16fd445e7b4b4b47902c363b9c33471996c
Trance-0 <60459821+Trance-0@users.noreply.github.com>
2021-05-24 14:41:42 +0800

    2021.5.24

---
+++ src/C32/H01267.java
@@ -0,0 +1,12 @@
+//http://oj.leaplearner.com/problem/01267
+package C32;
+
+public class H1267 {
+    public H1267() {
+
+    }
+
+    public static void main(String[] args) {
+        H1267 a = new H1267();
+    }
+}
