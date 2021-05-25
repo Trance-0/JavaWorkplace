97fe3afb63a72a6e43f7203b32e5de971bccee22
Trance-0 <60459821+Trance-0@users.noreply.github.com>
2021-05-25 15:48:46 +0800

    2021.5.25

--- WordNet/src/Main/SAP.java
+++ WordNet/src/Main/SAP.java
@@ -1,13 +1,24 @@
 package Main;
 import edu.princeton.cs.algs4.*;
 public class SAP {
+	private Digraph graph;
+	private int size;
 	// constructor takes a digraph (not necessarily a DAG)
 	public SAP(Digraph G) {
-		
+		graph=G;
+		size=G.V();
 	}
 	// length of shortest ancestral path between v and w; -1 if no such path
 	public int length(int v, int w){
+		int l=-1;
+		int[] checked=new int[size];
+		int note=1;
+		while(graph.indegree(v)!=0&&graph.indegree(w)!=0){
+			if(){
 
+			}
+			checked[]
+		}
 		return v;
 	}
 	// a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
---
+++ WordNet/src/Main/wordnettests.java
@@ -0,0 +1,44 @@
+package Main;
+
+import edu.princeton.cs.algs4.Digraph;
+
+import java.util.HashMap;
+
+public class wordnettests {
+        private Digraph graph;
+        private int size;
+    // constructor takes the name of the two input files
+    public wordnettests(String synsets, String hypernyms) {
+        if(synsets==null||hypernyms==null) {
+            throw new IllegalArgumentException();
+        }
+
+    }
+    private int findAncestor(int a) {
+        return 0;
+    }
+    // returns all WordNet nouns
+    public Iterable<String> nouns(){
+        return null;
+    }
+    // is the word a WordNet noun?
+    public boolean isNoun(String word) {
+        return false;
+    }
+    // distance between nounA and nounB (defined below)
+    public int distance(String nounA, String nounB) {
+        if(nounA==null||nounB==null||!isNoun(nounA)||!isNoun(nounB)) {
+            throw new IllegalArgumentException();
+        }
+        return -1;
+    }
+    // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
+// in a shortest ancestral path (defined below)
+    public String sap(String nounA, String nounB) {
+       return null;
+    }
+    // do unit testing of this class
+    public static void main(String[] args) {
+
+    }
+}
