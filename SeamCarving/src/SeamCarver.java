23736e8746464290d43581fce269333f7f9849f0
Trance-0 <60459821+Trance-0@users.noreply.github.com>
2021-06-02 16:42:14 +0800

    2021.6.2

--- SeamCarving/src/SeamCarver.java
+++ SeamCarving/src/SeamCarver.java
@@ -1,30 +1,50 @@
 import edu.princeton.cs.algs4.Picture;
 
-public class SeamCarver {
+import java.awt.Color;
+import java.util.PriorityQueue;
 
+public class SeamCarver {
+    private Picture pic;
+    private PriorityQueue<Integer> xdir;
+    private PriorityQueue<Integer> ydir;
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture){
-
+    pic=picture;
     }
 
     // current picture
     public Picture picture(){
-        return null;
+        return pic;
     }
 
     // width of current picture
     public int width(){
-        return 0;
+        return pic.width();
     }
 
     // height of current picture
     public int height(){
-        return 0;
+        return pic.height();
     }
 
     // energy of pixel at column x and row y
     public double energy(int x, int y){
-        return 0;
+        double e=0;
+        if(isBorder(x,y)){
+            return 1000;
+        }
+        else{
+            Color down=pic.get(x,y+1);
+            Color up=pic.get(x,y-1);
+            Color left=pic.get(x-1,y);
+            Color right=pic.get(x+1,y);
+            double part1=Math.pow(down.getRed()-up.getRed(),2)+Math.pow(down.getGreen()-up.getGreen(),2)+Math.pow(down.getBlue()-up.getBlue(),2);
+            double part2=Math.pow(left.getRed()-right.getRed(),2)+Math.pow(left.getGreen()-right.getGreen(),2)+Math.pow(left.getBlue()-right.getBlue(),2)
+            return Math.sqrt(part1+part2);
+        }
+    }
+    private boolean isBorder(int x,int y){
+    return x==width()||x==0||y==height()||y==0;
     }
 
     // sequence of indices for horizontal seam
