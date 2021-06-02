1d5ba61c5172947b9eb582fafd2f1a9ed156c6b1
Trance-0 <60459821+Trance-0@users.noreply.github.com>
2021-06-02 08:44:13 +0800

    2021.6.2

--- src/C33/H01281.java
+++ src/C33/H01281.java
@@ -13,86 +13,80 @@ public class H01281 {
     private int N;
     private int V;
     private int edgeSize;
-    private LinkedList<int[]>[]tree;
-    private boolean[]cowPos;
+    private int[] cowPos;
     private int[][] E;
     private int seed;
-    public H01281() throws IOException{
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
-        N=Integer.parseInt(st.nextToken());
-        V=Integer.parseInt(st.nextToken());
-        edgeSize=Integer.parseInt(st.nextToken());
-        cowPos=new boolean[V];
-        
-        st=new StringTokenizer(br.readLine());
-        seed=Integer.parseInt(st.nextToken());
-        cowPos[seed]=true;
 
-        for(int i=1;i<N;i++){
-            st=new StringTokenizer(br.readLine());
-            cowPos[Integer.parseInt(st.nextToken())-1]=true;
-        }
-        E=new int[edgeSize][3];
-        for(int i=0;i<edgeSize;i++){
-            st=new StringTokenizer(br.readLine());
-            E[i][0]=Integer.parseInt(st.nextToken())-1;
-            E[i][1]=Integer.parseInt(st.nextToken())-1;
-            E[i][2]=Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(E,new Comparator<int[]>(){
+    public H01281() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
+        edgeSize = Integer.parseInt(st.nextToken());
+        cowPos = new int[N];
 
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                // TODO Auto-generated method stub
-                return o1[2]-o2[2];
-            }
-            
-        });
-        tree=new LinkedList[V];
-        for (int j=0; j<V; j++){
-    tree[j]=new LinkedList<int[]>();
-    }
-        tree[E[0][0]].add(E[0]);
-        for(int i=0;i<V-1;i++){
-            for(int[] j:E){
-                if((tree[j[0]].isEmpty()&&!tree[j[1]].isEmpty())||(!tree[j[0]].isEmpty()&&tree[j[1]].isEmpty())){
-                    tree[j[0]].add(j);
-                    tree[j[1]].add(j);
-                    break;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            cowPos[i] = Integer.parseInt(st.nextToken()) - 1;
+        }
+        E = new int[V][V];
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (i != j) {
+                    E[i][j] = Integer.MAX_VALUE;
                 }
             }
         }
-        int sum=0;
-        int distance=0;
-        boolean[] isIngroup=new boolean[V];
-        isIngroup[seed]=true;
-        LinkedList<Integer>stack=new LinkedList<Integer>();
-        stack.add(seed);
-        //deep first search, return the value when reached an end and the end is a cow pos;
-        while(!stack.isEmpty()){
-            boolean isEnd=false;
-            for(int[] i:tree[stack.peekLast()]){
-                if(isIngroup[i[0]]==false||isIngroup[i[1]]==true){
-                    stack.add(i[0]);
-                    isIngroup[i[0]]=true;
-                    distance+=i[2];
-                }else if(isIngroup[i[0]]==true||isIngroup[i[1]]==false){
-                    stack.add(i[1]);
-                    isIngroup[i[1]]=true;
-                    distance+=i[2];
+
+        for (int i = 0; i < edgeSize; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken()) - 1;
+            int to = Integer.parseInt(st.nextToken()) - 1;
+            int weight = Integer.parseInt(st.nextToken());
+            if(E[from][to]>weight){
+                E[from][to] = weight;
+                E[to][from] = weight;
                 }
-            }
-            if(isEnd){
-                if(cowPos[stack.peekLast()]){
-                    sum+=distance;
+        }
+        // I love floy-warshall
+        for (int i = 0; i < V; i++) {
+            for (int j = i; j < V; j++) {
+                for (int k = 0; k < V; k++) {
+                    if (E[i][j] > E[i][k] + E[k][j] && E[i][k] + E[k][j] > 0) {
+                        E[i][j] = E[i][k] + E[k][j];
+                        E[j][i] = E[i][k] + E[k][j];
+                    }
                 }
-                stack.pollLast();
-                distance=0;
             }
         }
+        int sweetspot = Integer.MAX_VALUE;
+        for (int i[] : E) {
+            int temp = 0;
+            for (int j : cowPos) {
+                temp += i[j];
+            }
+            if (temp < sweetspot) {
+                sweetspot = temp;
+            }
+        }
+        System.out.println(sweetspot);
     }
-    public static void main(String[] args)throws IOException {
-        H01281 a=new H01281();
+
+    public String printarr(int[][] arr) {
+        StringBuilder sb = new StringBuilder();
+        for (int[] i : arr) {
+            sb.append("[");
+
+            for (int j : i) {
+                sb.append(j);
+                sb.append(",");
+            }
+            sb.append("]" + "\n");
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        H01281 a = new H01281();
     }
 }
